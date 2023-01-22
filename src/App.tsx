c876dc9f7d6d20fc47a18642104e4b548aef7fc3
DimaOstanin <xnadab@gmail.com>
2023-01-22 21:21:28 +0200

    error 22/01

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,35 @@
 
 import { Input } from './components/input';
 import React from 'react';
-import { Timer } from './components/Timer';
+
+
 
 
 function App() {
-  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
-  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
-  justifyContent: "space-around", width: "50vw", marginTop: "4vh" ,marginLeft:"20vw"}
-  
-  return <div style={flexColumn}>
-    <div style={flexRow}>
-      <Timer cityOrCountry={"London"} inputFieldId={'1'}></Timer>  
-      <Timer cityOrCountry={"Paris"} inputFieldId={'2'}></Timer>
-    </div>
+  const properties: React.CSSProperties = {
+    display: "flex",
+    flexWrap: "wrap"
+  }
+  const [colors,setcolors] = React.useState<string[]>([])
+function creatingDivs(value:string): string {
+    const colors: string[] = value.split("#") ;// red#green => ['red,'green']
+    setcolors(colors.slice());
+    return '';
+}
+function getDivs(colors: string[]): JSX.Element[]{
+  return colors.map(color => <div style={{width:"5vw" , height:"5vh",
+backgroundColor:color}}></div>)
+}
 
-    <div style={flexRow}>
-      <Timer cityOrCountry={"India "} inputFieldId={'1'}></Timer>
-      <Timer cityOrCountry={"Cuba"} inputFieldId={'3'}></Timer>
-      </div>
+return <section style={{display: "flex",flexDirection:"column",}}>
+   <Input placeHolder={'enter color separated by #'} inputProcess={creatingDivs}> </Input>
+    <section style={properties}>
+   {getDivs(colors)}
+    </section>
 
-    </div>
-    
+  </section>
+
 }
 export default App;
 
 
-// import React from 'react';
-// import { Input } from './components/Input';
-
-
-
-// function App() {
-//   function processInput(value: string): string {
-//     let res: string = '';
-//     if(value.toLowerCase().includes("hello")) {
-//         res = "get tired from HELLO. It's forbidden word";
-//     }
-//     return res;
-    
-//   }
-//   return <Input inputId={'input-1'} inputProcess={processInput} />
-
-// }
-
-// export default App;

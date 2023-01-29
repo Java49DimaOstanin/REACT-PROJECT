4d4637436e55ffa5f08b007a0650e7855aabd274
DimaOstanin <xnadab@gmail.com>
2023-01-29 18:55:54 +0200

    36HW random elements with new type T

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,32 +1,32 @@
 
  export function getRandomNumber(min:number ,max:number,isMinInclusive:boolean=true,isMaxInclusive:boolean=false):number{
-    if(min == max){
+    if((min == max)&& (!isMinInclusive || !isMaxInclusive)){
         throw new Error('min may not be equaled to max');
     }
     if(max < min){
-        let temp:number;
-        temp = max;
-        max =  min;
-        min = temp;
+        [min,max]=[max,min]
     }
     let minInclusive:number = isMinInclusive? 0:1;
     let maxInclusive:number = isMaxInclusive? 1:0;
     max+=maxInclusive;
     min+=minInclusive;
-    let randomN:number = Math.floor(Math.random()*(max - min) + min)
+    // let randomN:number = Math.floor(Math.random()*(max - min) + min)
     
-    return randomN;
+    return Math.floor(Math.random()*(max - min) + min);
 }
 export function getRandomMatrix(rows:number,colums:number,min:number,max:number):number[][]{
-    // let matrixArr:number[] = [Math.floor(Math.random()*(rows - colums) + colums)];
-    // let numberArr:number[] = [Math.floor(Math.random()*(max - min) + min)];
-    
-    let multArr:number[][] = [[Math.floor(Math.random()*(rows - colums) + colums)],[Math.floor(Math.random()*(max - min) + min)]];
+    const resMatrix:number[][] = [[]];
+    for(let i = 0;i< rows;i++){
+      resMatrix[i] = [];
+      for(let j = 0;j<colums;j++){
+        resMatrix[i][j] = getRandomNumber(min,max,true,true);
+      }
+    }
 
-    return multArr;
+    return resMatrix;
 }
-export function getRandomArrayElement(array:any[]):any{
-    return array[Math.floor(Math.random()*array.length)];
+export function getRandomArrayElement<T>(array:T[]):T{
+    return array[getRandomNumber(0,array.length)];
 }
 export function getRandomDate(minYear:number,maxYear:number):Date{
     const minDate:Date = new Date(minYear,0);
